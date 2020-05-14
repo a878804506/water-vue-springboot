53c92252684a196e52819cdf74c090a772b9be7c
252547545 <LIUfeng520>
2020-05-14 17:53:00 +0800

    第十九次提交：1、新增maven打包配置：打包成jar；2、修改统一根路径为：onecloud

--- flash-vue-admin/vue.config.js
+++ flash-vue-admin/vue.config.js
@@ -39,7 +39,7 @@ module.exports = {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
         // target: `http://localhost:${port}/mock`,
-        target:`http://172.16.0.177:8082/oneCloud`,//不使用mock模拟数据直接请求flash-api服务
+        target:`http://172.16.0.177:8082/onecloud`,//不使用mock模拟数据直接请求flash-api服务
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
