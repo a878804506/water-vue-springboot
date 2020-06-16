a3acc2b74b11aa183a73d83bed5d5a03f37c71f3
252547545 <LIUfeng520>
2020-06-16 18:30:22 +0800

    第三十一次提交：超管也可以修改密码

--- flash-api/src/main/java/cn/enilu/flash/api/controller/AccountController.java
+++ flash-api/src/main/java/cn/enilu/flash/api/controller/AccountController.java
@@ -169,9 +169,9 @@ public class AccountController extends BaseController {
                 return Rets.failure("新密码前后不一致");
             }
             User user = userService.get(getIdUser(HttpUtil.getRequest()));
-            if (ApiConstants.ADMIN_ACCOUNT.equals(user.getAccount())) {
+            /*if (ApiConstants.ADMIN_ACCOUNT.equals(user.getAccount())) {
                 return Rets.failure("不能修改超级管理员密码");
-            }
+            }*/
             logger.info("oldPassword:{},password:{},rePassword:{}", MD5.md5(oldPassword, user.getSalt()), password, rePassword);
             if (!MD5.md5(oldPassword, user.getSalt()).equals(user.getPassword())) {
                 return Rets.failure("旧密码输入错误");
