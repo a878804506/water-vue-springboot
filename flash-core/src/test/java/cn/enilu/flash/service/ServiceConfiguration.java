5db04642a013a7a6ff75c1913bfed2ab0e8b05a4
yanl@infoearth.com <159li00>
2020-07-31 18:11:39 +0800

    第五十三次提交：测试代码提交

--- flash-core/src/test/java/cn/enilu/flash/service/ServiceConfiguration.java
+++ flash-core/src/test/java/cn/enilu/flash/service/ServiceConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 */
-/**
+/** 测试
  * Name: ServiceConfiguration<br>
  * User: Yao<br>
  * Date: 2018/2/27<br>
